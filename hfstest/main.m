d56ae43c5b127ed627b9dad1cc942bc14fa5bac0
Adam Knight <adam@hopelessgeek.com>
2013-05-04 16:06:28 -0500

    Initial Commit

---
+++ hfstest/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  hfstest
+//
+//  Created by Adam Knight on 5/4/13.
+//  Copyright (c) 2013 Adam Knight. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
